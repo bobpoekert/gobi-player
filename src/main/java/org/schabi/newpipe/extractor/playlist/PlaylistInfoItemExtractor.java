b6093b266db5ae5de5d01d28578849c45d50d73c
Bob Poekert <bob@poekert.com>
2017-11-13 15:32:20 -0800

    delete all newpipe code

--- src/main/java/org/schabi/newpipe/extractor/playlist/PlaylistInfoItemExtractor.java
+++
@@ -1,9 +0,0 @@
-package org.schabi.newpipe.extractor.playlist;
-
-import org.schabi.newpipe.extractor.InfoItemExtractor;
-import org.schabi.newpipe.extractor.exceptions.ParsingException;
-
-public interface PlaylistInfoItemExtractor extends InfoItemExtractor {
-    String getUploaderName() throws ParsingException;
-    long getStreamCount() throws ParsingException;
-}
